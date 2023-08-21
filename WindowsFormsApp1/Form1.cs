4c03e3e9e70bfe20728b31ed336649bb8ad8a596
hoson <hoson@DESKTOP-A9ECVLR>
2023-08-21 23:15:57 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(username.Text != string.Empty && password.Text != string.Empty)
+            {
+                MessageBox.Show("login secceful ");
+            }
+            else
+            {
+                MessageBox.Show("plesse enter the try user name or password");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
